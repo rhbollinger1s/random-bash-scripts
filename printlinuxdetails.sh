862151037cc28008a0d2862dd10ccee62e5482d7
rhbollinger <rhbollinger1s@gmail.com>
2025-09-03 09:28:55 -0500

    Create printlinuxdetails.sh
    Just made a small bash script that prints out some details for when you start a bash session.

---
+++ printlinuxdetails.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#clear the screen
+clear
+
+#print details
+echo "You are signed in as: $USER."
+echo "Your working directory is $(pwd)."
+echo "The current time is "(date +"%Y-%m-%d)."
